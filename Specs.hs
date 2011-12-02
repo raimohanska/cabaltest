28e0937d24a1a30654a34ac6cf79114ddc7f0a15
Juha Paananen <juha.paananen@reaktor.fi>
2011-12-02 10:51:55 +0200

    simple HUnit integration

--- Specs.hs
+++ Specs.hs
@@ -1,1 +1,14 @@
-main = putStrLn "lol"
+import Test.HUnit
+import System.Exit(exitFailure)
+
+main = failOnError =<< runTestTT unitTests
+
+failOnError :: Counts -> IO ()
+failOnError (Counts _ _ 0 0) = return ()
+failOnError _                = exitFailure
+
+unitTests = test [
+  "makes sense" ~: do
+    putStrLn "testing"
+    assertEqual "1+1=2" 2 (1 + 1)
+  ]
